11688e357119ed43cf52ae9b94283220124ac0fa
Arlinton <felizarlinton@gmail.com>
2023-07-22 10:47:06 -0400

    Creacion de el campo de salida del texto

--- Calculadora.py
+++ Calculadora.py
@@ -3,5 +3,9 @@ from tkinter import *
 ventana = Tk()
 ventana.title("Calculadora")
 
+#entrada de datos
+e_texto = Entry(ventana, font=("calibri 20"))
+e_texto.grid(row=0, column= 0, columnspan= 4, padx= 5, pady= 5 )
+
 
 ventana.mainloop()
